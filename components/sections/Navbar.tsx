ba505c5da59ff09e51caf9cf9ba7cb943cfda0c4
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2023-11-30 23:32:36 +0800

    persist for theme switching

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,6 +1,14 @@
 import { FC } from "react"
 
 export const Navbar: FC = () => {
+  const switchTheme = () => {
+    if (localStorage.theme === "dark") {
+      localStorage.theme = "light"
+    } else {
+      localStorage.theme = "dark"
+    }
+  }
+
   return (
     // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
     <div className="fixed inset-x-0 z-40 mx-auto w-full border border-neutral bg-neutral bg-opacity-50 font-semibold sm:mt-4 sm:w-fit sm:rounded-full" style={{ backdropFilter: "blur(16px)" }}>
@@ -13,13 +21,13 @@ export const Navbar: FC = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-6 w-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
           
-        <li className="hidden sm:block"><a className="rounded-full hover:bg-transparent hover:text-primary">Home</a></li>
-        <li className="hidden sm:block"><a className="rounded-full hover:bg-transparent hover:text-primary">Blog</a></li>
+        <li className="hidden sm:block"><a href="https://www.kevinpek.com" className="rounded-full hover:bg-transparent hover:text-primary">About</a></li>
+        <li className="hidden sm:block"><a href="https://blog.kevinpek.com" className="rounded-full hover:bg-transparent hover:text-primary">Blog</a></li>
 
         {/* dark mode button - stays for all screen sizes */}
         <label className="btn swap border-none hover:text-primary sm:rounded-full sm:bg-neutral">
           {/* hidden checkbox controlling state */}
-          <input type="checkbox" className="theme-controller" value="light" />
+          <input type="checkbox" className="theme-controller" onClick={switchTheme} value="light" />
 
           {/* moon icon */}
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="swap-off h-6 w-6 fill-current">
@@ -38,8 +46,8 @@ export const Navbar: FC = () => {
         <label htmlFor="nav-drawer" aria-label="close sidebar" className="drawer-overlay"></label> 
         <ul className="menu min-h-full w-80 bg-base-200 p-4">
           {/* Sidebar content here */}
-          <li><a>Home</a></li>
-          <li><a>Blog</a></li>
+          <li><a href="https://www.kevinpek.com">About</a></li>
+          <li><a href="https://blog.kevinpek.com">Blog</a></li>
         </ul>
       </div>
     </div>
