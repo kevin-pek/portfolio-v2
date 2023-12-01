5fdb9eb59dd95212d9689de3ce50530a8a9c44d2
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2023-12-02 01:29:22 +0800

    icon changes

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,8 +15,6 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: "any",
         type: "image/x-icon",
       },
-      {"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
-      {"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}
     ],
   }
 }
