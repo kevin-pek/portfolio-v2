108a8792cb42dbd5c85ec1409a69ae9fb777a661
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-01-20 16:01:02 +0800

    Try fix statusbar theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-      </head>
       <body className={GeistSans.className}>{children}</body>
       <Analytics />
     </html>
--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -7,8 +7,10 @@ export const Navbar: FC = () => {
   
   useEffect(() => {
     if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+      document.documentElement.classList.add("dark")
       setTheme("dark")
     } else {
+      document.documentElement.classList.remove("dark")
       setTheme("light")
     }
   }, [])
