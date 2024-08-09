465502cc5f6cc2b456611453ab9944c179d2b168
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2024-08-10 03:30:59 +0800

    Update experience

--- components/sections/Experiences.tsx
+++ components/sections/Experiences.tsx
@@ -5,15 +5,15 @@ export const Experiences = () => {
     <div className="flex flex-col gap-6 px-4 md:px-24">
       <h1 className="text-base-content">Experience</h1>
 
-      <Accordion title="Data Scientist Intern @ GovTech" skills={["Python", "FastAPI", "PyTorch", "PaddlePaddle", "sklearn", "Deep Learning", "AWS", "Docker"]}>
+      <Accordion title="Data Scientist Intern @ GovTech" skills={["Python", "FastAPI", "PyTorch", "PaddlePaddle", "sklearn", "Docker", "Deep Learning", "AWS"]}>
         Applied few shot learning technique for CLIP model. Finetuned multimodal encoder model on Sagemaker. Trained and deployed machine learning models. Optimised response time for API running deployed models.
       </Accordion>
 
-      <Accordion title="Research Assistant @ NUS IORA, NCS" skills={["Python", "Data Analysis", "Azure", "Finite Volume Method", "FiPy", "PyTorch"]}>
+      <Accordion title="Research Assistant @ NUS IORA, NCS" skills={["Python", "Data Analysis", "Finite Volume Method", "FiPy", "PyTorch", "Azure"]}>
         Exploratory data analysis on time series data using Python. Created thermal render of heat distribution within Data Center using the Finite Volume Method and Heat Equation. Trained forecasting model for predicting temperature and power usage in Data Center.
       </Accordion>
 
-      <Accordion title="Research Assistant @ NUS IORA, SIA" skills={["Typescript", "Express", "React", "Docker", "Python", "FastAPI", "Multi-armed Bandits"]}>
+      <Accordion title="Research Assistant @ NUS IORA, SIA" skills={["Typescript", "Express", "React", "Docker", "Python", "FastAPI", "Multi-armed Bandits", "AWS"]}>
         Full stack development for a quiz platform with question recommender system and dashboards. Handled deployment of services on AWS and on-premises. Written epsilon greedy algorithm for prototype recommender system and deployed it using FastAPI.
       </Accordion>
 
